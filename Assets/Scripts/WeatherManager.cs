26ddd334095307c40bece5effa9e2551c096aec6
solroo <solroo@gmail.com>
2019-11-02 16:06:34 +0100

    weather, cars

---
+++ Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour
+{
+    [Header("Links")]
+    [SerializeField] Camera mainCamera;
+
+    [Header("Fog")]
+    [SerializeField] bool useFog;
+    [SerializeField] Color fogColor;
+    [SerializeField] [Range(0,0.1f)]float fogDensity;
+
+    [Header("Rain")]
+    [SerializeField] float rainAmount;
+    [SerializeField] RainCameraController rainOnCamera;
+    [SerializeField] DigitalRuby.RainMaker.RainScript rain;
+    [SerializeField] bool rainExposed = true;
+
+    Vector3 cameraPrevPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        fogDensity = RenderSettings.fogDensity;
+        cameraPrevPosition = mainCamera.transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        RenderSettings.fog = useFog;
+        RenderSettings.fogColor = fogColor;
+        fogDensity = Mathf.Max(0.05f, (50 - mainCamera.transform.position.y) / 50f)*0.05f;
+        RenderSettings.fogDensity = fogDensity;
+        mainCamera.backgroundColor = fogColor;
+
+        //this is not the best to get the angle
+        Vector3 camPosition = mainCamera.transform.position;
+        Vector3 cameraVelocity = cameraPrevPosition - camPosition;
+        cameraPrevPosition = mainCamera.transform.position;
+        if (rainExposed)
+        {
+            if(!rainOnCamera.IsPlaying)
+            { rainOnCamera.Play(); }
+
+            rainOnCamera.GlobalWind = cameraVelocity;
+            rain.RainIntensity = rainAmount;
+        }
+        else
+        {
+            if (rainOnCamera.IsPlaying)
+            { rainOnCamera.Stop(); }
+
+            rain.RainIntensity = rainAmount * 0.9f;
+        }
+        rainOnCamera.Alpha = rainAmount;
+    }
+}
