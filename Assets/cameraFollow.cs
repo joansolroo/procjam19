81c0c4ab4cf4d3e0f88218a7dee2e5621c539751
Pierre-Antoine Cinquin <pierre-antoine.cinquin@u-bordeaux.fr>
2019-11-05 23:28:16 +0100

    Start work for new 3C

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    [Header("Target")]
+    public Transform target;
+
+    [Space]
+
+    [Header("Offset")]
+    public Vector3 offset = Vector3.zero;
+
+    [Space]
+
+    [Header("Limits")]
+    public Vector2 limits = new Vector2(5, 3);
+
+    [Space]
+
+    [Header("Smooth Damp Time")]
+    [Range(0, 1)]
+    public float smoothTime;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        //FollowTarget(target);
+    }
+
+    void LateUpdate()
+    {
+        Vector3 localPos = transform.localPosition;
+        transform.localPosition = new Vector3(Mathf.Clamp(localPos.x, -limits.x, limits.x), Mathf.Clamp(localPos.y, -limits.y, limits.y), localPos.z);
+    }
+
+    public void FollowTarget(Transform t)
+    {
+        Vector3 localPos = transform.localPosition;
+        Vector3 targetLocalPos = t.transform.localPosition;
+        transform.localPosition = Vector3.SmoothDamp(localPos, new Vector3(targetLocalPos.x + offset.x, targetLocalPos.y + offset.y, targetLocalPos.z), ref velocity, smoothTime);
+    }
+}
