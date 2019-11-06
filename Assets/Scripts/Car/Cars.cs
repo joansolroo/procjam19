78f2e92ad48dd530fe372ed2d4baf0902ea44872
Pierre-Antoine Cinquin <pierre-antoine.cinquin@u-bordeaux.fr>
2019-11-06 17:42:25 +0100

    Started Building Car random generator
    Done in a new script, so I don't break anything :p

---
+++ Assets/Scripts/Car/Cars.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cars : Particle
+{
+
+    public GameObject[] carFrontTemplate;
+    public GameObject[] carMidTemplate;
+    public GameObject[] carBackTemplate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateCars();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateCars()
+    {
+        // Pick a random front
+        int frontIndex = Random.Range(0, carFrontTemplate.Length);
+        GameObject front = Instantiate(carFrontTemplate[frontIndex]);
+        // Pick a random mid
+        int midIndex = Random.Range(0, carMidTemplate.Length);
+        GameObject mid = Instantiate(carMidTemplate[midIndex]);
+        // Pick a random front
+        int backIndex = Random.Range(0, carBackTemplate.Length);
+        GameObject back = Instantiate(carBackTemplate[backIndex]);
+    }
+
+    protected override void DoCreate() { }
+    protected override void DoTick() { }
+    protected override void DoDestroy() { }
+}
