52ac508a9841f592d4c22e1098c9b10af14037e4
Pierre-Antoine Cinquin <pierre-antoine.cinquin@u-bordeaux.fr>
2019-11-09 10:17:39 +0100

    Script to control soundfx

---
+++ Assets/lowpass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lowpass : MonoBehaviour
+{
+    [SerializeField] Transform car;
+    AudioLowPassFilter filter;
+
+    public float freq = 20000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        filter = GameObject.Find("Test_song").GetComponent<AudioLowPassFilter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        freq = car.localPosition.y;
+        filter.cutoffFrequency = freq;
+    }
+}
