5debe563643a2ac9fb5c3a35abc3e7c35eaf994a
Pierre-Antoine Cinquin <pierre-antoine.cinquin@u-bordeaux.fr>
2019-11-09 11:54:38 +0100

    tweak low pass control

--- Assets/lowpass.cs
+++ Assets/lowpass.cs
@@ -6,8 +6,11 @@ public class lowpass : MonoBehaviour
 {
     [SerializeField] Transform car;
     AudioLowPassFilter filter;
+    
 
     public float freq = 20000;
+    public float min_freq_cut = 1000; // minimum low cut freq
+    public float freq_mul = 170;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +20,7 @@ public class lowpass : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        freq = car.localPosition.y;
-        filter.cutoffFrequency = freq;
+        //freq = car.localPosition.y * freq;
+        filter.cutoffFrequency = min_freq_cut + car.localPosition.y * freq_mul;
     }
 }
